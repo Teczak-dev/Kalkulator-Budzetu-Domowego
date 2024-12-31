28435f8c8125e46a9a57c8fb107afe6c18045b79
Mikołaj Sobczak <teczaksupermiki@gmail.com>
2024-12-31 15:26:46 +0100

    Initial Commit

---
+++ Kalkulator Budzetu Domowego/Kalkulator_Budzetu_DomowegoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kalkulator_Budzetu_DomowegoApp.swift
+//  Kalkulator Budzetu Domowego
+//
+//  Created by Mikołaj Sobczak on 31/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Kalkulator_Budzetu_DomowegoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kalkulator Budzetu DomowegoTests/Kalkulator_Budzetu_DomowegoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Kalkulator_Budzetu_DomowegoTests.swift
+//  Kalkulator Budzetu DomowegoTests
+//
+//  Created by Mikołaj Sobczak on 31/12/2024.
+//
+
+import Testing
+@testable import Kalkulator_Budzetu_Domowego
+
+struct Kalkulator_Budzetu_DomowegoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
